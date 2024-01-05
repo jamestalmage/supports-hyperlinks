31c23d33b8b4d8eec753f1c9849c31f99d674505
WillForan <willforan@gmail.com>
2024-01-04 20:09:18 -0500

    check $TERM to add alacritty support

--- test.js
+++ test.js
@@ -180,6 +180,16 @@ test('supported in VTE 5105 (0.51.5)', t => {
 		}
 	}));
 });
+test('supported in alacritty', t => {
+	t.true(isSupported({
+		env: {
+			TERM: 'alacritty',
+		},
+	}));
+});
+test('empty env not supported', t => {
+	t.false(isSupported({env: {}}));
+});
 
 test.failing('no-color flag disables support', t => {
 	t.false(isSupported({
